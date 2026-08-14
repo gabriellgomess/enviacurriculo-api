a1a545ce0a11a5b15f25be9826f5d1672e48565f
Gabriel Gomes <gabriel.gomes@outlook.com>
2026-08-14 10:44:57 -0300

    backup as 03h

--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 // Agendamento diário de backup automático do banco de dados na VPS às 03:00 com retenção de 5 dias
-// Schedule::command('app:backup-db --days=5')->dailyAt('03:00');
-Schedule::command('app:backup-db --days=5')->everyMinute();
+Schedule::command('app:backup-db --days=5')->dailyAt('03:00');
+// Schedule::command('app:backup-db --days=5')->everyMinute();
